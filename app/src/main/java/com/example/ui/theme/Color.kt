f7c0f40f7c385c6ee45a5aa66b049a256cc9acd3
DEV MESSY <techdevask@gmail.com>
2026-08-05 20:40:45 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WhatsAppTeal = Color(0xFF128C7E)
+val WhatsAppGreen = Color(0xFF25D366)
+val WhatsAppDarkTeal = Color(0xFF075E54)
+val WhatsAppLightBg = Color(0xFFE5DDD5)
+val WhatsAppCardBg = Color(0xFFF7F8FA)
+val WhatsAppDarkBg = Color(0xFF111B21)
+val WhatsAppDarkSurface = Color(0xFF202C33)
+
+val PrimaryGreen = Color(0xFF00A884)
+val SecondaryGreen = Color(0xFF128C7E)
+val AccentGreen = Color(0xFF25D366)
+
