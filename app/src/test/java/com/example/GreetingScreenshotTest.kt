0f843e23b5a322270630cf858e2bdd47dd5edbb2
DEV MESSY <techdevask@gmail.com>
2026-08-05 21:04:08 +0100

    Add files via upload

---
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import com.example.ui.theme.WebMessengerTheme
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+import androidx.compose.material3.Text
+
+@RunWith(RobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [36])
+class GreetingScreenshotTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Test
+  fun greeting_screenshot() {
+    composeTestRule.setContent { WebMessengerTheme { Text("Web Messenger") } }
+
+    composeTestRule.onRoot().captureRoboImage(filePath = "src/test/screenshots/greeting.png")
+  }
+}
